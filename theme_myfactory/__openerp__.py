425ab26c3dfd66808fe8a22811de0d69cc3023d6
chang <nobrother@gmail.com>
2017-03-09 18:29:00 +0800

    preparation

---
+++ theme_myfactory/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*
+
+{
+    'name': 'Theme: myfactory',
+    'version': '1.0.0',
+    'summary': '',
+    'description': '',
+    'category': 'theme',
+    'author': 'ohmycode',
+    'website': '',
+    'license': '',
+    'depends': [''],
+    'data': [''],
+    'demo': [''],
+    'installable': True,
+    'auto_install': False,
+}
